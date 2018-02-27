919794c00100f7380b769169fe9511f90c8f90e0
Madhu <enometh@net.meer>
2018-02-27 16:12:05 +0530

    examples: tmp localfixes to compile

--- examples/flood-game/load.lisp
+++ examples/flood-game/load.lisp
@@ -1,12 +1,18 @@
 (in-package :cl-user)
-
-(ql:quickload '("cl-gobject-introspection"
+#+nil
+(ql:quickload '("cffi"
+                "cffi-objects"
+                "cl-gobject-introspection"
                 ;; Accessible Cairo API through gobject introspection
                 ;; is quite poor
                 "cl-cairo2"))
-
+(require 'cl-cairo2)
+(require 'cffi-objects)
 (let ((*default-pathname-defaults*
         (merge-pathnames #P"examples/flood-game/"
+			 #+(and (not asdf) mk-defsystem)
+			 (make::system-relative-pathname :cl-gir "")
+			 #+asdf
                          (asdf:system-source-directory "cl-gobject-introspection"))))
   (load #P"./src/package.lisp")
   (load #P"./src/gui.lisp")
@@ -29,8 +35,28 @@
                   (1 1 2 3 3 2 2 1 0 3 1 2))))
 
 
+#+nil
 (visualize-solution-in-gtk-window
  '(3 2 1 2 0 1 3 2 3 0 2 1 0 3 2))
+
+(require 'bordeaux-threads)
+(defvar $t1
+  (bordeaux-threads:make-thread
+   (lambda ()
+     (gtk-window-with-cairo-painting 'render-image-in-cairo-context))
+   :name "Flood Game GUI"))
+
+(defvar $t2
+  (bordeaux-threads:make-thread
+   (lambda ()
+     (visualize-solution-in-gtk-window
+      '(3 2 1 2 0 1 3 2 3 0 2 1 0 3 2)))
+   :name "Flood Visualize-Solution"))
+
+
+#+nil
+(gtk-window-with-cairo-painting #'render-image-in-cairo-context)
+
 
 ;; (gtk-window-with-cairo-painting 'render-image-in-cairo-context)
 ;; or without blocking REPL with slime-eval-defun or:
