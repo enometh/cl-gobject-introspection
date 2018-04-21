faa966e58d67b3c1f951103a1d6784965787327c
Madhu <enometh@net.meer>
2018-04-21 19:49:55 +0530

    ECL/MKCL crash tmpfix
    * src/function.lisp: (mem-get (t stringpointer-type)): ECL and MKCL both
    crash:
    
    One failure case is:
    
    (defvar *gdkpixbuf* (require-namespace "GdkPixbuf"))
    (setq $l (invoke ((nget  *gdkpixbuf* "Pixbuf") "get_formats")))
    (setq $s (gir::build-struct-ptr (nget *gdkpixbuf* "PixbufFormat")$l))
    (get-method-desc (nget *gdkpixbuf* "PixbufFormat") "get_name")
    ;; => #F<get_name(): (#V<RETURN-VALUE: STRING>)>
    
    (invoke ($s "get_name"))
    ;; Illegal :UTF-8 character starting at position 0.
    ;;   [Condition of type BABEL-ENCODINGS:INVALID-UTF8-STARTER-BYTE]
    
    * src/function.lisp: bandage another mem-get ECL crash
    
    Without this fix mem-get (pos (type pointer-type)) crashes on the
    following. Why? No clue yet.
    
    (defvar $win-class-ptr
      (cffi:foreign-funcall "g_type_class_peek"
    			:ulong (gir::gtype-of (nget *gtk* "Window"))
    			:pointer))
    (defvar $win-class (gir::build-object-ptr (nget *gobject* "ObjectClass")
    					  $win-class-ptr))
    (invoke ($win-class "list_properties"))

--- src/function.lisp
+++ src/function.lisp
@@ -140,6 +140,8 @@
 	   (value (if pointed-type
 		      (mem-get ptr pointed-type)
 		      ptr)))
+;; FIXME
+      #-(or ecl mkcl)
       (if (and (free-from-foreign-p type)
 	       (free-from-foreign-aggregated-p pointed-type))
 	  (cffi:foreign-free ptr))
@@ -450,7 +452,7 @@
   (let* ((ptr (cffi:mem-ref pos :pointer))
 	 (str (cffi:foreign-string-to-lisp ptr)))
     (when (free-from-foreign-p string-pointer-type)
-      #-ecl
+      #-(or ecl mkcl)
       (cffi:foreign-string-free ptr))
     str))
 
