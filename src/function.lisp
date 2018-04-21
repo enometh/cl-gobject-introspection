9b3e5d0614197d6d19f56016e66e073e10fc756f
Madhu <enometh@net.meer>
2018-04-21 20:05:12 +0530

    src/function.lisp: fix length-arg

--- src/function.lisp
+++ src/function.lisp
@@ -687,7 +687,6 @@
    (direction :reader direction-of)
    (for-array-length-p :initform nil :accessor for-array-length-p-of)
    (array-length :reader array-length-of)
-   (ugly-offset :initform nil :accessor ugly-offset)
    (caller-allocates)
    (transfer)))
 
@@ -820,18 +819,12 @@
 	     :then (make-arg data inp outp voutp))
 	(collect arg)))
 
-(defun ugly-nth (data array-length args)
-  (nth (if (ugly-offset data)
-	   (1+ array-length)
-	   array-length)
-       args))
-
 (defun arg-setup-length (arg args methodp)
   (with-slots (data length-arg)
       arg
     (with-slots (array-length)
 	data
-      (setf length-arg	    
+      (setf length-arg
 	    (if array-length (elt+ args array-length methodp))))))
 
 (defun in-arg-setup (arg value)
@@ -868,7 +861,7 @@
       (when (eq direction :in)
 	(let ((real-type
 	       (if is-array-type
-		   (copy-find-set-c-array-type-length 
+		   (copy-find-set-c-array-type-length
                       type
                       (when length-arg (slot-value length-arg 'value)))
 		   type)))
