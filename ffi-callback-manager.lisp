94013567f4c66f3af0cde3c2f4b9b37654b070a1
Madhu <enometh@net.meer>
2019-09-29 10:42:14 +0530

    ffi-callback-manager.lisp: new monster macro define-foreach-callback-mapper

--- ffi-callback-manager.lisp
+++ ffi-callback-manager.lisp
@@ -43,3 +43,36 @@
   `(let ((,loc-var (register-callback ,function)))
      (unwind-protect (progn ,@body)
        (unregister-callback ,loc-var))))
+
+;;Define XXX-MAP-CALLBACK MAP-XXX-1 and (MAP-XXX result-type function
+;;cffi-ptr)
+(defmacro define-foreach-callback-mapper (c-func-name c-type-name)
+  (let ((cffi-callback-name (intern (concatenate 'string (string-upcase c-type-name) "-MAP-CALLBACK")))
+	(ptr-var-name (intern (concatenate 'string (string-upcase c-type-name) "-PTR")))
+	(helper-function-name (intern (concatenate 'string "MAP-" (string-upcase c-type-name) "-1")))
+	(map-function-name  (intern (concatenate 'string "MAP-" (string-upcase c-type-name)))))
+    `(progn
+       (cffi:defcallback ,CFFI-CALLBACK-NAME :void
+	   ((data :pointer) (user-data :pointer))
+	 (let ((thunk (find-callback user-data)))
+	   (with-simple-restart (skip-execution ,(format nil "Skip Executing one ~A" c-func-name))
+	     (funcall thunk data))))
+
+       (defun ,HELPER-FUNCTION-NAME (,PTR-VAR-NAME function)
+	 (with-registered-callback (loc) function
+	   (cffi:foreign-funcall
+	    ,c-func-name
+	    :pointer ,PTR-VAR-NAME
+	    :pointer (CFFI-SYS:%CALLBACK ',CFFI-CALLBACK-NAME)
+	    :pointer loc)))
+
+       (defun ,MAP-FUNCTION-NAME (result-type function ,PTR-VAR-NAME)
+	 (let* ((ret nil)
+		(func (if result-type
+			  #'(lambda (x) (push (funcall function x) ret))
+			  function)))
+	   (,HELPER-FUNCTION-NAME ,PTR-VAR-NAME func)
+	   (if result-type (coerce (nreverse ret) result-type)))))))
+
+#+nil
+(define-foreach-callback-mapper "g_slist_foreach" "SLIST")
