bd132b1d4608a6a218d81d43ae7eb03b16d26efe
Madhu <enometh@net.meer>
2019-10-06 05:44:30 +0530

    src/clos-obj: fix recompilation of all files by splitting it up
    * src/clos-obj-pkg.lisp: new. compile and load before src/clos-obj.lisp
    Arrange to define validate-superclass in the correct package. (there is
    no validate superclas in mkcl)
    
    * src/clos-obj.pkg: move some functions out to the new file

---
+++ src/clos-obj-pkg.lisp
@@ -0,0 +1,24 @@
+(in-package "GIR")
+
+(defun eval-1 (expr)
+  (format t "EVAL-1: ===> ~S~&" expr)
+  (with-simple-restart (skip "Skip this form")
+    (eval expr)))
+
+(defun intern-1 (string package)
+  (let ((symbol-name (string-upcase (substitute #\- #\_ string))))
+    (or (find-symbol symbol-name package)
+	(progn (format t "INTERN-1: ~A -> ~A~&" string symbol-name)
+	       (intern symbol-name  package)))))
+
+(defun %build-validate-superclass-forms (class)
+  (let ((sym (or  #+(or lispworks ecl clisp) 'clos:validate-superclass
+		  #+(or allegro cmu) 'mop:validate-superclass
+		  #+(or clozure) 'ccl:validate-superclass
+		  #+(or mkcl) 'gir::validate-superclass ;BOGUS
+		  #+(or sbcl) 'sb-mop:validate-superclass
+		  (error "No validate-superclass"))))
+  `(progn (defmethod ,sym ((class ,class) (super standard-class))
+	    t)
+	  (defmethod ,sym ((class standard-class) (super ,class))
+	    t))))
