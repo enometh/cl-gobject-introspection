c9f2c169ddeeeb8709a42e70b6a12ad4027eedc6
Madhu <enometh@net.meer>
2020-05-20 19:29:14 +0530

    gdbus-connection.lisp: handle separate system and session connection variables

--- gdbus-connection.lisp
+++ gdbus-connection.lisp
@@ -14,22 +14,35 @@
 ;;; SINGLETON CONNECTION
 ;;;
 
-(defvar *connection* nil "GDBusConnection")
+(defvar *dbus-system-connection* nil "GDBusConnection to the system bus")
+(defvar *dbus-session-connection* nil "GDBusConnection to the system bus")
 
 (defun dbus-init-bus (&optional bus)
   "Establish the connection to D-Bus. BUS can be either :SYSTEM or
 :SESSION. Defaults to :SESSION."
-  (unless *connection*
-    (setq *connection*
-	  (gir:invoke (*gio* "bus_get_sync")
-		      (gir::nget *gio* "BusType" (ecase bus
-						   ((:session :system) bus)
-						   ((nil) :session)))
-		      nil))
-    (setf (gir:property *connection* "exit-on-close") nil))
-  (assert (not (gir:invoke (*connection* "is_closed"))))
-  (values *connection*
-	  (gir:invoke (*connection* "get_unique_name"))))
+  (let* ((bus (ecase bus
+		((:session :system) bus)
+		((nil) :session)))
+	 (bus-connection-var (ecase bus
+			       (:session  '*dbus-session-connection*)
+			       (:system '*dbus-system-connection*)))
+	 (connection nil))
+    (unless (setq connection (symbol-value bus-connection-var))
+      (setq connection
+	    (set bus-connection-var
+		 (gir:invoke (*gio* "bus_get_sync")
+			     (gir::nget *gio* "BusType" bus)
+			     nil)))
+      (setf (gir:property connection "exit-on-close") nil))
+    (assert (not (gir:invoke (connection "is_closed"))))
+    (values connection
+	    (gir:invoke (connection "get_unique_name")))))
+
+(defun dbus-system-bus ()
+  (or *dbus-system-connection* (dbus-init-bus :system)))
+
+(defun dbus-session-bus ()
+  (or *dbus-session-connection* (dbus-init-bus :session)))
 
 #+nil
 (dbus-init-bus)
