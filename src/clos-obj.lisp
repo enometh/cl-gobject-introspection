96ba0a13894ddb1d70c2cbc4c4f05f7a1bfb772a
Madhu <enometh@net.meer>
2021-07-18 20:48:24 +0530

    src/clos-obj.lisp: (gobject-new): new. + other comments.

--- src/clos-obj.lisp
+++ src/clos-obj.lisp
@@ -62,6 +62,9 @@
 		 interface-base-class))
   (eval-1 (%build-validate-superclass-forms class)))
 
+;; ;madhu 210718 this only works for one namespace. if there are more
+;; than one namespaces then *package* should be bound to a package for
+;; the appropriate namespce
 (defvar *class-to-object-class-hash* (make-hash-table))
 
 ;; return a symbol denoting the class-name. recursively define classes
@@ -144,6 +147,9 @@
 	   ,doc
 	   (gir:invoke (self ,string-name) ,@(mapcar #'car args))))))
 
+;; ;madhu 210718 this doesn't "scale" at all. it might be possible to
+;; salvage ensure-class though, and have a means to define foreign
+;; gobject subclasses
 (defun %define-methods (thing)
   (%ensure-class thing)
   (when (typep thing 'object-class)
@@ -159,9 +165,71 @@
 #||
 (mapcar 'shadow '(close atom map remove))
 (import 'gir-test::*gtk*)
-(%ensure-class (nget *gtk* "Window") :force-p t)
+(%ensure-class (nget gir-test::*gtk* "Window") :force-p t)
 (%define-methods (nget *gtk* "Window"))
 (setq $win (new 'window (nget *gtk* "WindowType" :toplevel)))
 (gir-lib:with-gtk-thread (show-all $win))
 ||#
 
+
+;;; ----------------------------------------------------------------------
+;;;
+;;;
+;;;
+(export 'gobject-new)
+
+(defun gobject-new (object-gtype &rest key-val-pairs)
+  (let* ((n-properties (/ (length key-val-pairs) 2))
+	 (names (progn (assert (integerp n-properties))
+		       (cffi:foreign-alloc :string :count n-properties)))
+	 (values
+	  (cffi:foreign-alloc '(:struct g-value-struct) :count n-properties))
+	 (object-class-ptr
+	  (let ((object-class-ptr
+		 (cffi:foreign-funcall "g_type_class_peek" :ulong object-gtype
+				       :pointer)))
+	    (if (cffi:null-pointer-p object-class-ptr)
+		 (cffi:foreign-funcall "g_type_class_ref"
+				       :ulong object-gtype
+				       :pointer)
+		 object-class-ptr)))
+	 ret)
+    (assert (not (cffi:null-pointer-p object-class-ptr)))
+    (loop for i from 0
+	  for (prop-name lisp-val) on key-val-pairs by #'cddr
+	  for g-param-spec = (g-object-class-find-property
+			      object-class-ptr prop-name)
+	  for prop-gtype = (cffi:foreign-slot-value
+			    g-param-spec
+			    '(:struct g-param-spec) 'spec-type)
+	  for gvalue-ptr = (make-gvalue prop-gtype lisp-val)
+	  do
+	  (setf (cffi:mem-aref names :string i)
+		prop-name)
+	  (setf (cffi:mem-aref values '(:struct g-value-struct) i)
+		(cffi:mem-ref		;ugh
+		 gvalue-ptr '(:struct g-value-struct)))
+	  (cffi:foreign-free gvalue-ptr)
+	  finally (setq ret (cffi:foreign-funcall
+			     "g_object_new_with_properties"
+			     :ulong object-gtype
+			     :uint n-properties
+			     :pointer names
+			     :pointer values
+			     :pointer)))
+    (cffi:foreign-free names)
+    (cffi:foreign-free values)
+    (gobject object-gtype ret)))
+
+#||
+(list-property-names (nget gir-test::*gio* "Application"))
+(setq $a0 (gobject-new (gtype-of (nget gir-test::*gio* "Application"))
+		       "application-id"  "org.robolove.dummy"
+		       "flags" 0))
+(list-all-props $a0)
+(%ensure-class (nget gir-test::*gio* "Application") :force-p t)
+(gethash 'Application *class-to-object-class-hash*)
+(setq $a1 (change-class $a0 'application))
+(eql $a1 $a0)
+(list-all-props $a1)
+||#
