171b6bde6aa52d408f4f136d31f0aab3df0c65bf
Madhu <enometh@net.meer>
2022-02-13 07:22:12 +0530

    squash! gtk-thread.lisp: try to handle mkcl locks in bordeaux-threads
    default to recursive-locks for ecl too

--- gtk-thread.lisp
+++ gtk-thread.lisp
@@ -14,8 +14,8 @@
   (thread nil)
   (queue (make-array 0 :adjustable t :fill-pointer t)) ;task-queue
   (source-id nil)     ;glib main-loop source to process the task-queue
-  (lock (#+mkcl bordeaux-threads:make-recursive-lock
-	 #-mkcl
+  (lock (#+(or mkcl ecl) bordeaux-threads:make-recursive-lock
+	 #-(or mkcl ecl)
 	 bordeaux-threads:make-lock
 	 "gtk-task-queue-lock"))
   (free-list nil))		  ; indices of removed thunks in queue
