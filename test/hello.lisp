c052297519aac6420990714a76718b16f94aeb22
Madhu <enometh@net.meer>
2022-08-28 11:40:16 +0530

    test/hello.lisp: (WIP) adapt for gtk4
    TEMPORARILY - presently :wk in features indicates gtk-3

--- test/hello.lisp
+++ test/hello.lisp
@@ -2,7 +2,12 @@
   (:use #:cl))
 (in-package #:gir-test)
 
-(defvar *gtk* (gir:ffi "Gtk"))
+(defvar *gtk* (gir:ffi "Gtk" #-wk "4.0" #+wk "3.0"))
+
+#-wk
+(defvar *glib* (gir:require-namespace "GLib"))
+#-wk
+(defvar *done* nil)
 
 (cffi:defcallback hello :void ((btn-ptr :pointer))
   (let ((button (gir::build-object-ptr (gir:nget *gtk* "Button") btn-ptr)))
@@ -10,25 +15,46 @@
   (format t "Hello, pressed~%"))
 
 (defun main ()
-  (gir:invoke (*gtk* 'init) nil)
+  (gir:invoke (*gtk* 'init) #+wk nil)
   (let ((window (gir:invoke (*gtk* "Window" 'new)
+		             #+wk
 			    (gir:nget *gtk* "WindowType" :toplevel)))
-        (button (gir:invoke (*gtk* "Button" 'new-with-label) "Hello, world!")))
+        (button (gir:invoke (*gtk* "Button" 'new-with-label) "Hello, world!"))
+	#-wk
+	(context (gir:invoke (*glib* "main_context_default")))
+	)
+    #+wk
     (gir::g-signal-connect-data (gir::this-of window)
                                 "destroy"
                                 (cffi:foreign-symbol-pointer "gtk_main_quit")
                                 (cffi:null-pointer)
                                 (cffi:null-pointer)
                                 0)
+
+    #-wk
+    (gir:connect window "unrealize"
+		 (lambda (&rest args)
+		   (setq *done* t)
+		   (gir:invoke (context "wakeup"))))
+
     ;; (gir::g-signal-connect-data (gir::this-of button)
     ;;                             "clicked"
     ;;                             (cffi:callback hello)
     ;;                             (cffi:null-pointer)
     ;;                             (cffi:null-pointer)
     ;;                             0)
-    (gir:connect button :clicked 
+
+    (gir:connect button :clicked
                  (lambda (button)
-                   (setf (gir:property button 'label) "OK")))
-    (gir:invoke (window 'add) button)
-    (gir:invoke (window 'show-all))
-    (gir:invoke (*gtk* 'main))))
+                   (setf (gir:property button 'label) "OK")
+		   (gir:invoke (window "close"))))
+
+    (gir:invoke (window #+wk 'add #-wk "set_child") button)
+    (gir:invoke (window #+wk 'show-all #-wk "show"))
+
+    #+wk
+    (gir:invoke (*gtk* 'main))
+    #-wk
+    (loop initially  (setq *done* nil) while (not *done*)
+	  do (gir:invoke (context "iteration") t))))
+
